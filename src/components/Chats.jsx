67f53abf39664f0b65bc19ff82945a8d43c702ff
victor.mbarachi@halogen-group.com <39647480+Mbarachi@users.noreply.github.com>
2022-10-25 17:50:34 +0100

    Creating the messaging component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+      <div className="userChat">
+        <img src="https://media-exp1.licdn.com/dms/image/C5603AQE7VCTojWrTgA/profile-displayphoto-shrink_400_400/0/1617028820663?e=1672272000&v=beta&t=rSXIxNpAeXrrXM_g33B5cucnndqEdWEMD4RlU4WMeq8" alt="" />
+        <div className="userChatInfo">
+          <span>Victor</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://media-exp1.licdn.com/dms/image/C5603AQE7VCTojWrTgA/profile-displayphoto-shrink_400_400/0/1617028820663?e=1672272000&v=beta&t=rSXIxNpAeXrrXM_g33B5cucnndqEdWEMD4RlU4WMeq8" alt="" />
+        <div className="userChatInfo">
+          <span>Victor</span>
+          <p>Hey baby!</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://media-exp1.licdn.com/dms/image/C5603AQE7VCTojWrTgA/profile-displayphoto-shrink_400_400/0/1617028820663?e=1672272000&v=beta&t=rSXIxNpAeXrrXM_g33B5cucnndqEdWEMD4RlU4WMeq8" alt="" />
+        <div className="userChatInfo">
+          <span>Victor</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Search from '../components/Search'
+import Chats from '../components/Chats'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <NavBar/>
       <Search/>
+      <Chats/>
     </div>
   )
 }
